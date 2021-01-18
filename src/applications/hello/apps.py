1a0544c8af802f2383f01d8ccf690a3ecafafe52
Michael <52786686+mikepolo-JQ@users.noreply.github.com>
2021-01-19 01:32:21 +0300

    Delete src/applications/hello directory

--- src/applications/hello/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HelloConfig(AppConfig):
-    lable = "hello"
-    name = f"applications.{lable}"
--- src/applications/hello/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from applications.hello import views
-
-urlpatterns = [
-    path("", views.HelloView.as_view(), name="hello"),
-    path("reset/", views.HelloClearView.as_view()),
-]
